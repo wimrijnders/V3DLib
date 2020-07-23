8a30cad5cafe3bd9267e56431711732feb739704
Wim Rijnders <wrijnders@gmail.com>
2020-07-23 09:00:07 +0200

    Added more fields to v3d register mapping, especially for revision numbers

--- Lib/v3d/RegisterMapping.cpp
+++ Lib/v3d/RegisterMapping.cpp
@@ -1,4 +1,7 @@
-// Derived from: https://github.com/Idein/py-videocore6/blob/master/videocore6/v3d.py
+// Derived from:
+//
+// - https://github.com/Idein/py-videocore6/blob/master/videocore6/v3d.py
+// - https://github.com/torvalds/linux/blob/d15be546031cf65a0fc34879beca02fd90fe7ac7/drivers/gpu/drm/v3d/v3d_debugfs.c#L127
 //
 ///////////////////////////////////////////////////////////////////////////////
 #include <stdio.h>
@@ -12,8 +15,16 @@ namespace {
 enum: unsigned {  // NOTE: the pointers are to 4-bit words
 	HUB_IDENT1 = 0x0000c >> 2,
 
+	V3D_HUB_IDENT0 = 0x00008 >> 2,
+	V3D_HUB_IDENT1 = 0x0000c >> 2,
+	V3D_HUB_IDENT2 = 0x00010 >> 2,
+	V3D_HUB_IDENT3 = 0x00014 >> 2,
+
   CORE_BASE   = (0xfec04000 + 0x4000) >> 2,
-	CORE_IDENT1 = 0x00004 >> 2
+	CORE_IDENT0 = 0x00000,
+	CORE_IDENT1 = 0x00004 >> 2,
+	CORE_IDENT2 = 0x00008 >> 2,
+	V3D_CTL_MISCCFG = 0x00018 >> 2
 };
 
 
@@ -69,28 +80,75 @@ unsigned RegisterMapping::num_cores() {
 }
 
 
-RegisterMapping::CoreInfo RegisterMapping::core_info(unsigned core_index) {
+RegisterMapping::Info RegisterMapping::info() {
+	Info ret;
+
+	uint32_t V3D_HUB_IDENT2_WITH_MMU = 1 <<  8;
+	uint32_t V3D_HUB_IDENT1_WITH_L3C = 1 << 16;
+	uint32_t V3D_HUB_IDENT1_WITH_TFU = 1 << 17;
+  uint32_t V3D_HUB_IDENT1_WITH_TSY = 1 << 18;
+	uint32_t V3D_HUB_IDENT1_WITH_MSO = 1 << 19;
+
+	uint32_t ident0 = m_addr[V3D_HUB_IDENT0];
+	uint32_t ident1 = m_addr[V3D_HUB_IDENT1];
+	uint32_t ident2 = m_addr[V3D_HUB_IDENT2];
+	uint32_t ident3 = m_addr[V3D_HUB_IDENT3];
+
+	ret.num_cores = num_cores();
+
+	ret.tver    = HubField(ident1,  3, 0);
+	ret.rev     = HubField(ident1,  7, 4);
+	ret.iprev   = HubField(ident3, 15, 8);
+	ret.ipidx   = HubField(ident3,  7, 0);
+	ret.mmu     = (ident2 & V3D_HUB_IDENT2_WITH_MMU) != 0;
+	ret.tfu     = (ident1 & V3D_HUB_IDENT1_WITH_TFU) != 0;
+  ret.tsy     = (ident1 & V3D_HUB_IDENT1_WITH_TSY) != 0;
+	ret.mso	    = (ident1 & V3D_HUB_IDENT1_WITH_MSO) != 0;
+	ret.l3c     = (ident1 & V3D_HUB_IDENT1_WITH_L3C) != 0;
+	ret.l3c_nkb = HubField(ident2, 7, 0);
+
+	return ret;
+}
+
+
+RegisterMapping::CoreInfo RegisterMapping::info_per_core(unsigned core_index) {
 	CoreInfo ret;
 	ret.index = core_index;
 
-	uint32_t val  = *(map_cores[core_index] + CORE_IDENT1);
+	uint32_t val0     = *(map_cores[core_index] + CORE_IDENT0);
+	uint32_t val1     = *(map_cores[core_index] + CORE_IDENT1);
+	uint32_t val2     = *(map_cores[core_index] + CORE_IDENT2);
+	uint32_t misccfg  = *(map_cores[core_index] + V3D_CTL_MISCCFG);
 
-	unsigned CORE_IDENT1_VPM_SIZE = HubField(val, 31, 28);
-	unsigned CORE_IDENT1_NSEM = HubField(val, 23, 16);
-	unsigned CORE_IDENT1_NSLC = HubField(val, 7, 4);  // Num slices
-	unsigned CORE_IDENT1_REV = HubField(val, 3, 0);
+	unsigned CORE_IDENT0_VER = HubField(val0, 31, 24);
+
+	unsigned CORE_IDENT1_VPM_SIZE = HubField(val1, 31, 28);
+	unsigned CORE_IDENT1_NSEM = HubField(val1, 23, 16);
+	unsigned CORE_IDENT1_NSLC = HubField(val1, 7, 4);  // Num slices
+	unsigned CORE_IDENT1_REV = HubField(val1, 3, 0);
+
+	unsigned CORE_IDENT2_BCG = HubField(val2, 28, 28);
 
 	// Following values appear to be *per slice*:
 	// Source: https://github.com/torvalds/linux/blob/master/drivers/gpu/drm/v3d/v3d_debugfs.c#L151
-	unsigned CORE_IDENT1_NTMU = HubField(val, 15, 12);
-	unsigned CORE_IDENT1_QUPS = HubField(val, 11, 8);
+	unsigned CORE_IDENT1_NTMU = HubField(val1, 15, 12);
+	unsigned CORE_IDENT1_QUPS = HubField(val1, 11, 8);
+
+	unsigned CORE_MISCCFG_QRMAXCNT  = HubField(misccfg, 3, 1);
+	unsigned CORE_MISCCFG_OVRTMUOUT = HubField(misccfg, 0, 0);
+
 
 	unsigned nslc = CORE_IDENT1_NSLC;	
 
-	ret.vpm_size  = CORE_IDENT1_VPM_SIZE;
-	ret.num_slice = nslc;
-	ret.num_tmu   = CORE_IDENT1_NTMU * nslc;
-	ret.num_qpu   = CORE_IDENT1_QUPS * nslc;  // Hoping that this indeed means QPUS
+	ret.ver           = CORE_IDENT0_VER;
+	ret.rev           = CORE_IDENT1_REV;
+	ret.vpm_size      = CORE_IDENT1_VPM_SIZE;
+	ret.num_slice     = nslc;
+	ret.num_semaphore = CORE_IDENT1_NSEM;
+	ret.num_tmu       = CORE_IDENT1_NTMU * nslc;
+	ret.num_qpu       = CORE_IDENT1_QUPS * nslc;
+	ret.bcg_int       = (CORE_IDENT2_BCG != 0);
+	ret.override_tmu  = (CORE_MISCCFG_OVRTMUOUT != 0);
 
 	return ret;
 }
--- Lib/v3d/RegisterMapping.h
+++ Lib/v3d/RegisterMapping.h
@@ -14,15 +14,38 @@ public:
 
 	unsigned num_cores(); 
 
-	struct CoreInfo {
-		unsigned index      = 0;
-		unsigned vpm_size   = 0;
-		unsigned num_slice  = 0;
-		unsigned num_tmu    = 0;  // all slices
-		unsigned num_qpu    = 0;  // all slices
+	struct Info {
+		unsigned num_cores = 0; 
+
+		// version numbers, in given order
+		unsigned tver          = 0;
+		unsigned rev           = 0;
+		unsigned iprev         = 0;
+		unsigned ipidx         = 0;
+
+		bool     mmu           = false;
+		bool     tfu           = false;
+		bool     tsy           = false;
+		bool     mso           = false;
+		bool     l3c           = false;
+		unsigned l3c_nkb       = 0;     // Size of L3C in KB
 	};
 
-	CoreInfo core_info(unsigned core_index);
+	struct CoreInfo {
+		unsigned ver           = 0;
+		unsigned rev           = 0;
+		unsigned index         = 0;
+		unsigned vpm_size      = 0;
+		unsigned num_slice     = 0;
+		unsigned num_semaphore = 0;
+		unsigned num_tmu       = 0;  // all slices
+		unsigned num_qpu       = 0;  // all slices
+		bool     bcg_int       = false;
+		bool     override_tmu  = false;
+	};
+
+	Info info();
+	CoreInfo info_per_core(unsigned core_index);
 
 private:
   uint32_t *m_addr = nullptr;
