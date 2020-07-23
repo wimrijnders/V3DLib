df0203a4ee0babb1b49c1c239c9a92d9c6b614f0
Wim Rijnders <wrijnders@gmail.com>
2020-07-23 13:20:27 +0200

    Added Mesa to repo
    Succeeded in getting it compiled! Or at least the part relevant to `v3`.
    Current build compiles `v3d_compile()`.
    The minimum set of files has been included from the Mesa library to get this done.
    
    This is obviously not enough, but a decent first step.

---
+++ mesa/main/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "broadcom/compiler/v3d_compiler.h"
+
+void debug_output(const char *msg, void *debug_output_data) {
+}
+
+int main(int argc, char *argv[]) {
+
+	struct v3d_compiler compiler;
+	struct v3d_key key;
+	struct v3d_prog_data *prog_data = 0;
+	nir_shader s;
+	void *debug_output_data = 0;
+	int program_id = 0;
+	int variant_id = 0;
+	uint32_t final_assembly_size;
+
+	uint64_t *ret = v3d_compile(
+		&compiler,
+		&key,
+		&prog_data,
+		&s,
+    debug_output,
+		debug_output_data,
+		program_id, variant_id,
+		&final_assembly_size);
+
+	printf("ret: %llu\n", *ret);
+
+	return 0;
+}
