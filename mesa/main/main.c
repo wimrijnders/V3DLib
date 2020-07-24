91eb1d36749670b43056ae51332fcf3fd2e9dc7f
Wim Rijnders <wrijnders@gmail.com>
2020-07-24 19:35:43 +0200

    Added more of mesa to repo

--- mesa/main/main.c
+++ mesa/main/main.c
@@ -1,11 +1,20 @@
+/**
+ * Goal of this file is to initiate the proper linking for the required functions
+ *
+ */
 #include <stdio.h>
+#include "gallium/drivers/v3d/v3d_context.h"
+
+/*
 #include "broadcom/compiler/v3d_compiler.h"
 
 void debug_output(const char *msg, void *debug_output_data) {
 }
+*/
 
 int main(int argc, char *argv[]) {
 
+/*
 	struct v3d_compiler compiler;
 	struct v3d_key key;
 	struct v3d_prog_data *prog_data = 0;
@@ -26,6 +35,11 @@ int main(int argc, char *argv[]) {
 		&final_assembly_size);
 
 	printf("ret: %llu\n", *ret);
+*/
+
+	
+	struct v3d_context v3d;
+	v3d_update_compiled_cs(&v3d);
 
 	return 0;
 }
