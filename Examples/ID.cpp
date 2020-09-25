517fa9793df92d74ef30c8cf88c3130e0c7d0e62
Wim Rijnders <wrijnders@gmail.com>
2020-09-25 06:12:17 +0200

    ID example now also outputs index

--- Examples/ID.cpp
+++ Examples/ID.cpp
@@ -7,9 +7,10 @@ QPULib::Settings settings;
 
 // Define function that runs on the GPU.
 
-void id_kernel(Ptr<Int> p)
+void id_kernel(Ptr<Int> p, Ptr<Int> q)
 {
   *p = me();
+  *q = index();
 }
 
 
@@ -21,17 +22,21 @@ int main(int argc, const char *argv[]) {
   auto k = compile(id_kernel);
 
   // Allocate and initialise array shared between ARM and GPU
-  SharedArray<int> array(16*k.maxQPUs());
-  for (int i = 0; i < array.size(); i++)
-    array[i] = 0;
+  SharedArray<int> id_array(16*k.maxQPUs());
+  for (int i = 0; i < id_array.size(); i++)
+    id_array[i] = 0;
 
-  k.setNumQPUs(k.maxQPUs());  // Invoke the kernel
-	k.load(&array);             // Load the uniforms
+  SharedArray<int> index_array(16*k.maxQPUs());
+  for (int i = 0; i < index_array.size(); i++)
+    index_array[i] = 0;
+
+  k.setNumQPUs(k.maxQPUs());        // Invoke the kernel
+	k.load(&id_array, &index_array);  // Load the uniforms
 	settings.process(k);
 
 	// Display the result
-  for (int i = 0; i < array.size(); i++) {
-    printf("%i: %i\n", i, array[i]);
+  for (int i = 0; i < id_array.size(); i++) {
+    printf("%3i: %2i, %2i\n", i, id_array[i], index_array[i]);
   }
   
   return 0;
