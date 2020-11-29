ce78329dce33bc0ef26b9c753e2a90e9e5f3355c
Wim Rijnders <wrijnders@gmail.com>
2020-11-29 05:38:31 +0100

    Remove shared_ptr's from Stack again, too unweildy

--- Lib/Common/Stack.h
+++ Lib/Common/Stack.h
@@ -1,67 +1,70 @@
 #ifndef _V3DLIB_COMMON_STACK_H_
 #define _V3DLIB_COMMON_STACK_H_
-#include <memory>
 #include "Support/debug.h"
 
 namespace V3DLib {
 
+/**
+ * TODO: Perhaps replace this with `Seq` as underlying base class
+ */
 template <class T> class Stack {
 private:
 	class StackItem {
 	public:
-		std::unique_ptr<T> head;
-		std::unique_ptr<StackItem> tail;
+		T *head         = nullptr;;
+		StackItem *tail = nullptr;
 	};
 
 public:
-	Stack() {}
+	~Stack() { clear(); }
 
 	bool     empty() const { return m_size == 0; }
 	unsigned size()  const { return m_size; }
 
 	void push(T* x) {
-		std::unique_ptr<StackItem> nextTop;
-		nextTop.reset(new StackItem);
-		nextTop->head.reset(x);
-		nextTop->tail.swap(m_topItem);
-
-		m_topItem.swap(nextTop);
+		StackItem *nextTop = new StackItem;
+		nextTop->head = x;
+		nextTop->tail = m_topItem;
+		
+		m_topItem = nextTop;
 		m_size++;
 	}
 
-		T* pop() {
-      assert(m_size > 0);
-			T *ret = m_topItem->head.release();
-			m_topItem.reset(m_topItem->tail.release());
-      m_size--;
-			return ret;
+	T *pop() {
+		assert(m_size > 0);
+		assert(m_topItem != nullptr);
+		StackItem *oldTop = m_topItem;
+
+		m_topItem = oldTop->tail;
+		T *ret    = oldTop->head;
+
+		oldTop->head = nullptr;
+		oldTop->tail = nullptr;
+		delete oldTop;
+
+		m_size--;
+		return ret;
+	}
+
+	T *top() const {
+		assert(m_size > 0);
+		assert(m_topItem != nullptr);
+		assert(m_topItem->head != nullptr);
+		return m_topItem->head;
+	}
+
+	void clear() {
+		while (m_topItem != nullptr) {
+			T *p = pop();
+			delete p;
 		}
 
-    T* top() const {
-      assert(m_size > 0);
-      return m_topItem->head.get();
-    }
-
-    // Clear the stack
-    void clear() {
-			while (m_topItem.get() != nullptr) {
-				breakpoint
-				ppop();
-      }
-
-      assert(m_size == 0);
-    }
-
+		assert(m_size == 0);
+	}
 
 private:
-	unsigned int m_size = 0;
-	std::unique_ptr<StackItem> m_topItem;
-
-	void ppop() {
-		assert(m_size > 0);
-		m_topItem.reset(m_topItem->tail.release());
-		m_size--;
-	}
+	unsigned int m_size  = 0;
+	StackItem *m_topItem = nullptr;
 };
 
 }  // namespace V3DLib
