91f9014a3a8435dae0fe90d6c4d301d9e8b000f2
Wim Rijnders <wrijnders@gmail.com>
2021-01-17 07:02:53 +0100

    Fix in install.sh, removed cd to pwd

--- script/install.sh
+++ script/install.sh
@@ -1,8 +1,6 @@
 #! /bin/bash
 ######################################33
 
-BASE=`pwd`
-
 #
 # Pull in project CmdParameter and build
 # 
@@ -11,8 +9,7 @@ if [ ! -d "CmdParameter" ]; then
 	git clone https://github.com/wimrijnders/CmdParameter.git
 fi
 
-cd $BASE
-cd ../CmdParameter
+cd CmdParameter
 git checkout master
 git pull origin master
 make clean
