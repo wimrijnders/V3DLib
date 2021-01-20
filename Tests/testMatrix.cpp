90bf162a3129940f6375480331542f2a33cde733
wimrijnders <wrijnders@gmail.com>
2021-01-20 08:36:27 +0100

    Matrix unit tests working in interpreter mode. Some tab -> space cleanup

--- Tests/testMatrix.cpp
+++ Tests/testMatrix.cpp
@@ -11,21 +11,39 @@
 namespace {
 using namespace V3DLib;
 
+void dump(float *a, int size,  int linesize = -1) {
+  std::string str("<");
+
+  for (int i = 0; i < (int) size; i++) {
+
+    if (linesize != -1) {
+      if (i % linesize == 0) {
+        str << "\n";
+      }
+    }
+    str << a[i] << ", " ;
+  }
+
+  str << ">";
+  printf("%s\n", str.c_str());
+};
+
+
 void dump(SharedArray<float> &a, int linesize = -1) {
-	std::string str("<");
+  std::string str("<");
 
-	for (int i = 0; i < (int) a.size(); i++) {
+  for (int i = 0; i < (int) a.size(); i++) {
 
-		if (linesize != -1) {
-			if (i % linesize == 0) {
-				str << "\n";
-			}
-		}
-		str << a[i] << ", " ;
-	}
+    if (linesize != -1) {
+      if (i % linesize == 0) {
+        str << "\n";
+      }
+    }
+    str << a[i] << ", " ;
+  }
 
-	str << ">";
-	printf("%s\n", str.c_str());
+  str << ">";
+  printf("%s\n", str.c_str());
 };
 
 
@@ -38,17 +56,17 @@ void dump(SharedArray<float> &a, int linesize = -1) {
  * @param c  Pointer to result array
  */
 void matrix_mult_scalar(int N, float *c, float *a, float *b) {
-	for (int x = 0; x < N; x++) {
-		for (int y = 0; y < N; y++) {
-			float result = 0;
+  for (int x = 0; x < N; x++) {
+    for (int y = 0; y < N; y++) {
+      float result = 0;
 
-			for (int i = 0; i < N; i++) {
-				result += a[i + y*N] * b [x + i*N];
-			}
+      for (int i = 0; i < N; i++) {
+        result += a[i + y*N] * b [x + i*N];
+      }
 
-			c[x + y*N] = result;
-		}
-	}
+      c[x + y*N] = result;
+    }
+  }
 }
 
 
@@ -60,24 +78,24 @@ void matrix_mult_scalar(int N, float *c, float *a, float *b) {
  * This is a kernel helper function.
  */
 void rotate_sum(Float &input, Float &result) {
-	result = input;
+  result = input;
 
-	Float tmp = input;
-	for (int i = 0; i < 15; i++) {
-		tmp = rotate(tmp, 1);
-		//result += tmp;  // TODO this should work
-		result = result + tmp;
-	}
+  Float tmp = input;
+  for (int i = 0; i < 15; i++) {
+    tmp = rotate(tmp, 1);
+    //result += tmp;  // TODO this should work
+    result = result + tmp;
+  }
 }
 
 
 void check_sum(Ptr<Float> input, Ptr<Float> result) {
-	Float val = *input;
-	Float sum;
+  Float val = *input;
+  Float sum;
 
-	rotate_sum(val, sum);
+  rotate_sum(val, sum);
 
-	*result = sum;
+  *result = sum;
 }
 
 
@@ -91,19 +109,19 @@ void check_sum(Ptr<Float> input, Ptr<Float> result) {
  * @param n  index of vector element to set. Must be in range 0..15 inclusive
  */
 void set_at(Float &dst, Int n, Float &src) {
-	Where(index() == n)
-		dst = src;
-	End 
+  Where(index() == n)
+    dst = src;
+  End 
 }
 
 
 void check_set_at(Ptr<Float> input, Ptr<Float> result, Int index) {
-	Float a = *input;
-	Float b = *result;
+  Float a = *input;
+  Float b = *result;
 
-	set_at(b, index, a);
+  set_at(b, index, a);
 
-	*result = b;
+  *result = b;
 }
 
 /**
@@ -111,40 +129,43 @@ void check_set_at(Ptr<Float> input, Ptr<Float> result, Int index) {
  */
 class DotVector {
 public:
-	DotVector(int size) {
-		assertq(size >= 1, "There must be at least one element for DotVector");
-		elements.resize(size);	
-	}
+  DotVector(int size) {
+    assertq(size >= 1, "There must be at least one element for DotVector");
+    elements.resize(size);  
+  }
 
 
-	void load(Ptr<Float> input) {
-		for (int i = 0; i < (int) elements.size(); ++i) {
-			elements[i] = *input;  input += 16;
-		}
-	}
+  void load(Ptr<Float> input) {
+    for (int i = 0; i < (int) elements.size(); ++i) {
+      elements[i] = *input;  input += 16;
+    }
+  }
 
 
-	void save(Ptr<Float> output) {
-		for (int i = 0; i < (int) elements.size(); ++i) {
-			*output = elements[i];  output += 16;
-		}
-	}
+  void save(Ptr<Float> output) {
+    for (int i = 0; i < (int) elements.size(); ++i) {
+      *output = elements[i];  output += 16;
+    }
+  }
 
 
-	void dot_product(Ptr<Float> rhs, Float &result) {
-		Float tmp = 0;
+  /**
+   * Calculate the dot product of current instance and `rhs`.
+   *
+   * All vector elements of the result will contain the same value.
+   */
+  void dot_product(Ptr<Float> rhs, Float &result) {
+    Float tmp = 0;
 
-		for (int i = 0; i < (int) elements.size(); ++i) {
-			tmp = tmp + elements[i]*(*rhs);  rhs += 16;
-		}
+    for (int i = 0; i < (int) elements.size(); ++i) {
+      tmp = tmp + elements[i]*(*rhs);  rhs += 16;
+    }
 
-		Float tmp2;
-		rotate_sum(tmp, tmp2);
-		set_at(result, 0, tmp2);
-	}
+    rotate_sum(tmp, result);
+  }
 
 private:
-	std::vector<Float> elements;
+  std::vector<Float> elements;
 };
 
 
@@ -153,13 +174,16 @@ private:
  */
 template<int const N>
 void check_dotvector(Ptr<Float> dst, Ptr<Float> a, Ptr<Float> result) {
-	DotVector vec(N);
-	vec.load(a);
-	vec.save(dst);
+  DotVector vec(N);
+  vec.load(a);
+  vec.save(dst);
 
-	Float tmp = -2;  // Silly value for detection in unit test
-	vec.dot_product(a, tmp);
-	*result = tmp;
+  Float tmp = -2;  // Silly value for detection in unit test
+  Float tmp2;
+
+  vec.dot_product(a, tmp2);
+  set_at(tmp, 0, tmp2);
+  *result = tmp;
 }
 
 
@@ -169,209 +193,288 @@ void check_dotvector(Ptr<Float> dst, Ptr<Float> a, Ptr<Float> result) {
  */
 template<int const N>
 void test_dotvector() {
-	  SharedArray<float> a(16*N);
+  SharedArray<float> a(16*N);
 
-		for (int i = 0; i < (int) a.size(); i++) {
-			a[i] = 1.0f*((float ) (i + 1));
-		}
+  for (int i = 0; i < (int) a.size(); i++) {
+    a[i] = 1.0f*((float ) (i + 1));
+  }
 
-	  SharedArray<float> b(16*N);
-		b.fill(-1);
+  SharedArray<float> b(16*N);
+  b.fill(-1);
 
-	  SharedArray<float> result(16);
-		result.fill(-1.0f);
-		//dump(result);
+  SharedArray<float> result(16);
+  result.fill(-1.0f);
 
-		REQUIRE(a.size() == b.size());
+  REQUIRE(a.size() == b.size());
 
-		auto k = compile(check_dotvector<N>);
-		k.load(&b, &a, &result);
-		k.interpret();
+  auto k = compile(check_dotvector<N>);
+  k.load(&b, &a, &result);
+  k.interpret();
 
-		for (int i = 0; i < (int) a.size(); i++) {
-			REQUIRE(a[i] == b[i]);
-		}
+  for (int i = 0; i < (int) a.size(); i++) {
+    REQUIRE(a[i] == b[i]);
+  }
 
-		//dump(result);
+  // Calculate and check expected result of dot product
+  float expected = 0;
+  for (int i = 0; i < (int) a.size(); i++) {
+    expected += a[i]*a[i];
+  }
 
-		// Calculate and check expected result of dot product
-		float expected = 0;
-		for (int i = 0; i < (int) a.size(); i++) {
-			expected += a[i]*a[i];
-		}
+  for (int i = 0; i < (int) result.size(); i++) {
+    if (i == 0) {
+      INFO("result[0]: " << result[i]);
+      REQUIRE(result[i] == expected);
+    } else {
+      REQUIRE(result[i] == -2);
+    }
+  }
 
-		for (int i = 0; i < (int) result.size(); i++) {
-			if (i == 0) {
-				INFO("result[0]: " << result[i]);
-				REQUIRE(result[i] == expected);
-			} else {
-				REQUIRE(result[i] == -2);
-			}
-		}
-
-		// Do it again with simpler values for hand calculation
-		a.fill(1);
-		k.load(&b, &a, &result);
-		k.interpret();
-		REQUIRE(result[0] == 16*N);
+  // Do it again with simpler values for hand calculation
+  a.fill(1);
+  k.load(&b, &a, &result);
+  k.interpret();
+  REQUIRE(result[0] == 16*N);
 }
 
 
+template<int const N>
 void check_matrix_mult(Ptr<Float> dst, Ptr<Float> a, Ptr<Float> b) {
-	int const N = 1;
+  int const DIM = 16*N;
 
-	DotVector vec(N);
-	Float result;
+  DotVector vec(N);
+  Float result;
 
-	For (Int y = 0, y < 16*N, y++)
-		vec.load(a + 16*y);
+  For (Int y = 0, y < DIM, y++)
+    vec.load(a + DIM*y);
 
-		for (int x = 0; x < 16*N; ++x) {
-			//Int x_val = 2; //x;
-			Float tmp;
-			vec.dot_product(b + x*16, tmp);
-			//set_at(result, x_val, tmp);
-			result = tmp;
-		}
+    for (int x = 0; x < DIM; ++x) {
+      Float tmp;
+      vec.dot_product(b + DIM*x, tmp);
 
-		*dst = result;
-		dst += 16;
-	End
+      set_at(result, x % 16, tmp);
+
+      if (x % 16 == 15) {
+        *dst = result;
+        dst += 16;
+      }
+    }
+
+  End
 }
 
+
+
+/**
+ * Template parameters N is dimension of square matrix in block of 16 values
+ */
+template<int const N>
+void test_matrix_multiplication() {
+  int const SIZE = 16*N*16*N;
+
+  SharedArray<float> a(SIZE);
+  SharedArray<float> result(SIZE);
+
+  float a_scalar[SIZE];
+  for (int i = 0; i < SIZE; i++) {
+    a_scalar[i] = 1;
+  }
+
+  float expected[SIZE];
+  for (int i = 0; i < SIZE; i++) {
+    expected[i] = -1;
+  }
+  matrix_mult_scalar(16*N, expected, a_scalar, a_scalar);
+
+  for (int i = 0; i < SIZE; i++) {
+    REQUIRE(expected[i] == 16*N);
+  }
+
+  auto k = compile(check_matrix_mult<N>);
+  k.load(&result, &a, &a);
+
+  //
+  // Multiplication of empty input matrix
+  //
+  a.fill(0);
+  result.fill(-1);
+  k.interpret();
+
+  for (int i = 0; i < SIZE; i++) {
+    REQUIRE(result[i] == 0);
+  }
+
+  //
+  // Square of input matrix containing all ones
+  //
+  a.fill(1);
+  result.fill(-1);
+  k.interpret();
+
+  for (int i = 0; i < SIZE; i++) {
+    REQUIRE(result[i] == 16*N);
+  }
+
+  //
+  // Square of unit matrix
+  //
+  a.fill(0);
+  for (int i = 0; i < 16*N; i++) {
+    a[i + 16*N*i] = 1;
+  }
+
+  k.interpret();
+
+  //dump(expected, SIZE, 16);
+  //dump(result, 16*N);
+
+  for (int x = 0; x < 16*N; x++) {
+    for (int y = 0; y < 16*N; y++) {
+      if (x == y) {
+        REQUIRE(result[x + 16*N*y] == 1);
+      } else {
+        REQUIRE(result[x + 16*N*y] == 0);
+      }
+    }
+  }
+
+
+  //
+  // Random values in array
+  //
+  for (int i = 0; i < SIZE; i++) {
+    float val = (1.0f*(((float) (rand() % 200)) - 100.0f))/100.0f;  // Intention: values between -1 and 1
+    a[i] = val;
+    a_scalar[i] = val;
+  }
+
+  // b matrix needs to be transposed
+  SharedArray<float> b(SIZE);
+
+  for (int x = 0; x < 16*N; x++) {
+    for (int y = 0; y < 16*N; y++) {
+      b[y + 16*N*x] = a[x + 16*N*y];
+    }
+  }
+
+  matrix_mult_scalar(16*N, expected, a_scalar, a_scalar);
+
+  k.load(&result, &a, &b);
+  k.interpret();
+
+  float precision = 1e-5f;
+
+  for (int i = 0; i < SIZE; i++) {
+    REQUIRE(abs(result[i] - expected[i]) < precision);
+  }
+}
 
 }  // anon namespace
 
 
 TEST_CASE("Test Matrix algebra", "[matrix]") {
-	using namespace V3DLib;
-	Platform::use_main_memory(true);  // Run only interpreter and emulator for now
+  using namespace V3DLib;
+  Platform::use_main_memory(true);  // Run only interpreter and emulator for now
 
 
-	SECTION("Check scalar matrix multiplication") {
-		const int N = 16;  // Dimension of square matrix
+  SECTION("Check scalar matrix multiplication") {
+    const int N = 16;  // Dimension of square matrix
 
-		float a[N*N];
-		float b[N*N];
-		float c[N*N];
+    float a[N*N];
+    float b[N*N];
+    float c[N*N];
 
-		// Init matrices
-		for (int i = 0; i < N*N; i++) { a[i] =  1; }
-		for (int i = 0; i < N*N; i++) { b[i] =  2; }
-		for (int i = 0; i < N*N; i++) { c[i] = -1; }
+    // Init matrices
+    for (int i = 0; i < N*N; i++) { a[i] =  1; }
+    for (int i = 0; i < N*N; i++) { b[i] =  2; }
+    for (int i = 0; i < N*N; i++) { c[i] = -1; }
 
-		matrix_mult_scalar(N, c, a, b);
-		
-		for (int i = 0; i < N*N; i++) {
-			REQUIRE(c[i] == 32);
-		}
-	}
-
-
-	SECTION("Check rotate sum") {
-	  SharedArray<float> vec(16);
-		vec.fill(0.3f);
-
-		float expected = 0;
-		for (int i = 0; i < (int) vec.size(); i++) {
-			expected += vec[i];
-		}
-
-		REQUIRE(expected == 4.8f);
-
-	  SharedArray<float> result(16);
-		result.fill(-1);
-
-		auto k = compile(check_sum);
-
-		k.load(&vec, &result);
-		k.interpret();
-		REQUIRE(result[0] == 4.8f);
-
-		for (int i = 0; i < (int) vec.size(); i++) {
-			vec[i] = 0.1f*((float ) (i + 1));
-		}
-
-		k.load(&vec, &result);
-		k.interpret();
-		REQUIRE(result[0] == 0.1f*(16*17/2));
-		
-	}
+    matrix_mult_scalar(N, c, a, b);
+    
+    for (int i = 0; i < N*N; i++) {
+      REQUIRE(c[i] == 32);
+    }
+  }
 
 
-	SECTION("Check setting single vector element") {
-	  SharedArray<float> vec(16);
+  SECTION("Check rotate sum") {
+    SharedArray<float> vec(16);
+    vec.fill(0.3f);
 
-		for (int i = 0; i < (int) vec.size(); i++) {
-			vec[i] = 1.0f*((float ) (i + 1));
-		}
+    float expected = 0;
+    for (int i = 0; i < (int) vec.size(); i++) {
+      expected += vec[i];
+    }
 
-	  SharedArray<float> result(16);
-		result.fill(-1);
+    REQUIRE(expected == 4.8f);
 
-		auto k = compile(check_set_at);
-		k.load(&vec, &result, 0);
-		k.interpret();
+    SharedArray<float> result(16);
+    result.fill(-1);
 
-		for (int i = 0; i < (int) result.size(); i++) {
-			if (i == 0) {
-				REQUIRE(result[i] == vec[i]);
-			} else {
-				REQUIRE(result[i] == -1);
-			}
-		}
+    auto k = compile(check_sum);
 
-		k.load(&vec, &result, 7);
-		k.interpret();
+    k.load(&vec, &result);
+    k.interpret();
+    REQUIRE(result[0] == 4.8f);
 
-		for (int i = 0; i < (int) result.size(); i++) {
-			if (i == 0 || i == 7) {
-				REQUIRE(result[i] == vec[i]);
-			} else {
-				REQUIRE(result[i] == -1);
-			}
-		}
-	}
+    for (int i = 0; i < (int) vec.size(); i++) {
+      vec[i] = 0.1f*((float ) (i + 1));
+    }
+
+    k.load(&vec, &result);
+    k.interpret();
+    REQUIRE(result[0] == 0.1f*(16*17/2));
+    
+  }
 
 
-	SECTION("Check correct working dotvector") {
-		test_dotvector<2>();
-		test_dotvector<4>();
-		test_dotvector<10>();
-	}
+  SECTION("Check setting single vector element") {
+    SharedArray<float> vec(16);
+
+    for (int i = 0; i < (int) vec.size(); i++) {
+      vec[i] = 1.0f*((float ) (i + 1));
+    }
+
+    SharedArray<float> result(16);
+    result.fill(-1);
+
+    auto k = compile(check_set_at);
+    k.load(&vec, &result, 0);
+    k.interpret();
+
+    for (int i = 0; i < (int) result.size(); i++) {
+      if (i == 0) {
+        REQUIRE(result[i] == vec[i]);
+      } else {
+        REQUIRE(result[i] == -1);
+      }
+    }
+
+    k.load(&vec, &result, 7);
+    k.interpret();
+
+    for (int i = 0; i < (int) result.size(); i++) {
+      if (i == 0 || i == 7) {
+        REQUIRE(result[i] == vec[i]);
+      } else {
+        REQUIRE(result[i] == -1);
+      }
+    }
+  }
 
 
-	SECTION("Check matrix multiplication") {
-		int const N = 1;  // dimension of square matrix in block of 16 values;
-		int const SIZE = 16*N*16*N;
-
-		SharedArray<float> a(SIZE);
-		a.fill(1);
-
-		SharedArray<float> result(SIZE);
-		result.fill(-1);
-
-		float a_scalar[SIZE];
-		for (int i = 0; i < SIZE; i++) {
-			a_scalar[i] = 1;
-		}
-
-		float expected[SIZE];
-		for (int i = 0; i < SIZE; i++) {
-			expected[i] = -1;
-		}
-		matrix_mult_scalar(16*N, expected, a_scalar, a_scalar);
-
-		for (int i = 0; i < SIZE; i++) {
-			REQUIRE(expected[i] == 16);
-		}
-
-		auto k = compile(check_matrix_mult);
-		k.load(&result, &a, &a);
-		k.interpret();
-		dump(result, 16);
-	}
+  SECTION("Check correct working dotvector") {
+    test_dotvector<2>();
+    test_dotvector<4>();
+    test_dotvector<10>();
+  }
 
 
-	Platform::use_main_memory(false);
+  SECTION("Check matrix multiplication") {
+    test_matrix_multiplication<1>();
+    test_matrix_multiplication<2>();
+  }
+
+
+  Platform::use_main_memory(false);
 }
--- Tests/testRot3D.cpp
+++ Tests/testRot3D.cpp
@@ -20,35 +20,35 @@ void initArrays(Arr &x, Arr &y, int size, float mult = 1.0f) {
 
 template<typename Array1, typename Array2>
 void compareResults(
-	Array1 &x1,
-	Array1 &y1,
-	Array2 &x2,
-	Array2 &y2,
-	int size,
-	const char *label,
-	bool compare_exact = true) {
+  Array1 &x1,
+  Array1 &y1,
+  Array2 &x2,
+  Array2 &y2,
+  int size,
+  const char *label,
+  bool compare_exact = true) {
   for (int i = 0; i < size; i++) {
-		INFO("Comparing " << label << " for index " << i);
-		if (compare_exact) {
-			INFO("y2[" << i << "]: " << y2[i]);
-			REQUIRE(x1[i] == x2[i]);
+    INFO("Comparing " << label << " for index " << i);
+    if (compare_exact) {
+      INFO("y2[" << i << "]: " << y2[i]);
+      REQUIRE(x1[i] == x2[i]);
 
 /*
-			int exp;  // To avoid 0.0f == -0.0f (rhs is extremely small)
-			frexp(y2[i], &exp);
+      int exp;  // To avoid 0.0f == -0.0f (rhs is extremely small)
+      frexp(y2[i], &exp);
 
-			float rhs = y2[i];
-			if (exp != 0 && exp <= -100) {  // -126 <= exponent values <= 127
-				printf("rhs: %f, exp: %d \n", rhs, exp);
-				rhs = 0.0f;
-			}
-			REQUIRE(y1[i] == rhs);
+      float rhs = y2[i];
+      if (exp != 0 && exp <= -100) {  // -126 <= exponent values <= 127
+        printf("rhs: %f, exp: %d \n", rhs, exp);
+        rhs = 0.0f;
+      }
+      REQUIRE(y1[i] == rhs);
 */
-			REQUIRE(y1[i] == y2[i]);
-		} else {
-			REQUIRE(x1[i] == Approx(x2[i]).epsilon(0.001));
-			REQUIRE(y1[i] == Approx(y2[i]).epsilon(0.001));
-		}
+      REQUIRE(y1[i] == y2[i]);
+    } else {
+      REQUIRE(x1[i] == Approx(x2[i]).epsilon(0.001));
+      REQUIRE(y1[i] == Approx(y2[i]).epsilon(0.001));
+    }
   }
 }
 
@@ -63,90 +63,90 @@ TEST_CASE("Test working of Rot3D example", "[rot3d]") {
   const float THETA = (float) 3.14159;
 
 
-	/**
-	 * Check that the Rot3D kernels return precisely what we expect.
-	 *
-	 * The scalar version of the algorithm may return slightly different
-	 * values than the actual QPU's, but they should be close. This is
-	 * because the hardware QPU's round downward in floating point
-	 * calculations
-	 *
-	 * If the code is compiled for emulator only (QPU_MODE=0), this
-	 * test will fail.
-	 */
-	SECTION("All kernel versions should return the same") {
-		//
-		// Run the scalar version
-		//
+  /**
+   * Check that the Rot3D kernels return precisely what we expect.
+   *
+   * The scalar version of the algorithm may return slightly different
+   * values than the actual QPU's, but they should be close. This is
+   * because the hardware QPU's round downward in floating point
+   * calculations
+   *
+   * If the code is compiled for emulator only (QPU_MODE=0), this
+   * test will fail.
+   */
+  SECTION("All kernel versions should return the same") {
+    //
+    // Run the scalar version
+    //
 
-	  // Allocate and initialise
-	  float* x_scalar = new float [N];
-	  float* y_scalar = new float [N];
-		initArrays(x_scalar, y_scalar, N);
+    // Allocate and initialise
+    float* x_scalar = new float [N];
+    float* y_scalar = new float [N];
+    initArrays(x_scalar, y_scalar, N);
 
-	  rot3D(N, cosf(THETA), sinf(THETA), x_scalar, y_scalar);
+    rot3D(N, cosf(THETA), sinf(THETA), x_scalar, y_scalar);
 
-  	// Allocate and arrays shared between ARM and GPU
-	  SharedArray<float> x_1(N), y_1(N);
-	  SharedArray<float> x(N), y(N);
-
-
-		// Compare scalar with QPU output - will not be exact
-		{
-	  	auto k = compile(rot3D_1);
-			//k.pretty(true, "rot3D_1.txt");
-			initArrays(x_1, y_1, N);
-  		k.load(N, cosf(THETA), sinf(THETA), &x_1, &y_1).call();
-			compareResults(x_scalar, y_scalar, x_1, y_1, N, "Rot3D 1", false);  // Last param false: do approximate match
-		}
+    // Allocate and arrays shared between ARM and GPU
+    SharedArray<float> x_1(N), y_1(N);
+    SharedArray<float> x(N), y(N);
 
 
-		// Compare outputs of the kernel versions.
-		// These *should* be exact, because kernel 1 output is compared with kernel 2
- 		auto k2 = compile(rot3D_2);
-
-		{
-			initArrays(x, y, N);
-  		k2.load(N, cosf(THETA), sinf(THETA), &x, &y).call();
-			compareResults(x_1, y_1, x, y, N, "Rot3D_2");
-		}
-
-
-		if (Platform::instance().has_vc4) {
-			printf("NB: Rot3D kernel unit test for kernel 2 with `-n=8` not working on vc4\n");
-			// returns something but not complete
-			// e.g. x[16] not filled in
-			// works fine on v3d
-			// Also works fine for cmdline version Rot3D
-		} else {
-			INFO("Running with 8 kernels");
-  		k2.setNumQPUs(8);
-			initArrays(x, y, N);
-  		k2.load(N, cosf(THETA), sinf(THETA), &x, &y).call();
-			compareResults(x_1, y_1, x, y, N, "Rot3D_2 8 QPU's");
-		}
-
-		delete [] x_scalar;
-		delete [] y_scalar;
-	}
+    // Compare scalar with QPU output - will not be exact
+    {
+      auto k = compile(rot3D_1);
+      //k.pretty(true, "rot3D_1.txt");
+      initArrays(x_1, y_1, N);
+      k.load(N, cosf(THETA), sinf(THETA), &x_1, &y_1).call();
+      compareResults(x_scalar, y_scalar, x_1, y_1, N, "Rot3D 1", false);  // Last param false: do approximate match
+    }
 
 
-	SECTION("Multiple kernel definitions should be possible") {
-		if (!Platform::instance().has_vc4) {
-			printf("NB: Rot3D kernel unit test not working on v3d, timeout hang\n");
-			return;
-		}
+    // Compare outputs of the kernel versions.
+    // These *should* be exact, because kernel 1 output is compared with kernel 2
+     auto k2 = compile(rot3D_2);
 
-  	auto k_1 = compile(rot3D_1);
-  	SharedArray<float> x_1(N), y_1(N);
-		initArrays(x_1, y_1, N);
- 		k_1.load(N, cosf(THETA), sinf(THETA), &x_1, &y_1).call();
+    {
+      initArrays(x, y, N);
+      k2.load(N, cosf(THETA), sinf(THETA), &x, &y).call();
+      compareResults(x_1, y_1, x, y, N, "Rot3D_2");
+    }
 
-  	auto k_2 = compile(rot3D_2);
-  	SharedArray<float> x_2(N), y_2(N);
-		initArrays(x_2, y_2, N);
- 		k_2.load(N, cosf(THETA), sinf(THETA), &x_2, &y_2).call();
 
-		compareResults(x_1, y_1, x_2, y_2, N, "Rot3D_1 and Rot3D_2 1 QPU");
-	}
+    if (Platform::instance().has_vc4) {
+      printf("NB: Rot3D kernel unit test for kernel 2 with `-n=8` not working on vc4\n");
+      // returns something but not complete
+      // e.g. x[16] not filled in
+      // works fine on v3d
+      // Also works fine for cmdline version Rot3D
+    } else {
+      INFO("Running with 8 kernels");
+      k2.setNumQPUs(8);
+      initArrays(x, y, N);
+      k2.load(N, cosf(THETA), sinf(THETA), &x, &y).call();
+      compareResults(x_1, y_1, x, y, N, "Rot3D_2 8 QPU's");
+    }
+
+    delete [] x_scalar;
+    delete [] y_scalar;
+  }
+
+
+  SECTION("Multiple kernel definitions should be possible") {
+    if (!Platform::instance().has_vc4) {
+      printf("NB: Rot3D kernel unit test not working on v3d, timeout hang\n");
+      return;
+    }
+
+    auto k_1 = compile(rot3D_1);
+    SharedArray<float> x_1(N), y_1(N);
+    initArrays(x_1, y_1, N);
+     k_1.load(N, cosf(THETA), sinf(THETA), &x_1, &y_1).call();
+
+    auto k_2 = compile(rot3D_2);
+    SharedArray<float> x_2(N), y_2(N);
+    initArrays(x_2, y_2, N);
+     k_2.load(N, cosf(THETA), sinf(THETA), &x_2, &y_2).call();
+
+    compareResults(x_1, y_1, x_2, y_2, N, "Rot3D_1 and Rot3D_2 1 QPU");
+  }
 }
