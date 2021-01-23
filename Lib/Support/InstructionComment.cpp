748940c934b859237f075fc8872a3cebd27d4879
wimrijnders <wrijnders@gmail.com>
2021-01-23 09:03:14 +0100

    Added dump() method to StmtStack
    This in order to sanely view the stack contents during debugging.
    Turned out to be very useful.
    
    There very plenty of other files involved, committed today in
    other commits.
    This commit serves to log that this happened.

--- Lib/Support/InstructionComment.cpp
+++ Lib/Support/InstructionComment.cpp
@@ -1,27 +1,7 @@
 #include "InstructionComment.h"
 #include "Support/basics.h"
 
-namespace {
-
-/**
- * Source: https://thispointer.com/find-and-replace-all-occurrences-of-a-sub-string-in-c/
- */
-void findAndReplaceAll(std::string & data, std::string toSearch, std::string replaceStr) {
-    // Get the first occurrence
-    size_t pos = data.find(toSearch);
-    // Repeat till end is reached
-    while( pos != std::string::npos) {
-      // Replace this occurrence of Sub String
-      data.replace(pos, toSearch.size(), replaceStr);
-      // Get the next occurrence from the current position
-      pos =data.find(toSearch, pos + replaceStr.size());
-    }
-}
-
-}  // anon namespace
-
 namespace V3DLib {
-
 
 void InstructionComment::transfer_comments(InstructionComment const &rhs) {
   if (!rhs.header().empty()) {
