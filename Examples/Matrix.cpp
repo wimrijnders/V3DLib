33abb91a34693e1caa9444d011e5c6737e73e815
Wim Rijnders <wrijnders@gmail.com>
2021-01-24 06:10:03 +0100

    Added qpu kernel to matrix demo

--- Examples/Matrix.cpp
+++ Examples/Matrix.cpp
@@ -14,7 +14,7 @@ using namespace kernels;
 // Command line handling
 // ============================================================================
 
-std::vector<const char *> const kernel_id = { "cpu" };  // First is default
+std::vector<const char *> const kernel_id = { "qpu", "cpu" };  // First is default
 
 
 CmdParameters params = {
@@ -29,14 +29,16 @@ CmdParameters params = {
 
 
 struct MatrixSettings : public Settings {
-	int const N = 1;
+	static int const N    = 5;
+	static int const DIM  = 16*N;
+	static int const SIZE = DIM*DIM;
 
 	int    kernel;
 
 	MatrixSettings() : Settings(&params, true) {}
 
 	void init_params() override {
-		kernel              = params.parameters()["Kernel"]->get_int_value();
+		kernel = params.parameters()["Kernel"]->get_int_value();
 	}
 } settings;
 
@@ -46,37 +48,32 @@ struct MatrixSettings : public Settings {
 // Local functions
 // ============================================================================
 
-/*
-void run_qpu_kernel(KernelType &kernel) {
-	Timer timer;
-
-  auto k = compile(kernel);  // Construct kernel
+void run_qpu_kernel() {
+  auto k = compile(kernels::matrix_mult<MatrixSettings::N>);  // Construct kernel
   k.setNumQPUs(settings.num_qpus);
 
   // Allocate and initialise arrays shared between ARM and GPU
-  SharedArray<float> x(SIZE), y(SIZE);
-	init_arrays(x, y);
+  SharedArray<float> a(MatrixSettings::SIZE);
+  SharedArray<float> b(MatrixSettings::SIZE);
+  SharedArray<float> result(MatrixSettings::SIZE);
 
-  k.load(SIZE, cosf(THETA), sinf(THETA), &x, &y);
+  for (int i = 0; i < MatrixSettings::SIZE; i++) {
+    a[i] = random_float();
+    b[i] = random_float();
+  }
+
+  k.load(&result, &a, &b);
   settings.process(k);
-
-	timer.end(!settings.silent);
 }
-*/
 
 
 void run_scalar_kernel() {
-	int const DIM  = 16*settings.N;
-	int const SIZE = DIM*DIM;
-
-	Timer timer;
-
   // Allocate and initialise
-  float a[SIZE];
-  float b[SIZE];
-  float result[SIZE];
+  float a[MatrixSettings::SIZE];
+  float b[MatrixSettings::SIZE];
+  float result[MatrixSettings::SIZE];
 
-  for (int i = 0; i < SIZE; i++) {
+  for (int i = 0; i < MatrixSettings::SIZE; i++) {
     a[i] = random_float();
     b[i] = random_float();
   }
@@ -84,22 +81,23 @@ void run_scalar_kernel() {
 	if (!settings.compile_only) {
 		kernels::matrix_mult_scalar(settings.N, result, a, b);
 	}
-
-	timer.end(!settings.silent);
 }
 
 
 /**
  * Run a kernel as specified by the passed kernel index
  */
-void run_kernel(int kernel_index) {
-	switch (kernel_index) {
-		case 0: run_scalar_kernel(); break;
-		//case 1: run_qpu_kernel(rot3D_2);  break;	
-		//case 2: run_qpu_kernel(rot3D_1);  break;	
+void run_kernel() {
+	Timer timer;
+
+	switch (settings.kernel) {
+		case 0: run_qpu_kernel();    break;	
+		case 1: run_scalar_kernel(); break;
 	}
 
-	auto name = kernel_id[kernel_index];
+	auto name = kernel_id[settings.kernel];
+
+	timer.end(!settings.silent);
 
 	if (!settings.silent) {
 		printf("Ran kernel '%s' with %d QPU's.\n", name, settings.num_qpus);
@@ -115,7 +113,7 @@ int main(int argc, const char *argv[]) {
 	auto ret = settings.init(argc, argv);
 	if (ret != CmdParameters::ALL_IS_WELL) return ret;
 
-	run_kernel(settings.kernel);
+	run_kernel();
 
   return 0;
 }
