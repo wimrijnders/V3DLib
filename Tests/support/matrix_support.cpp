48c231a9cc6968c2fe6b59488a4dcea47016461f
Wim Rijnders <wrijnders@gmail.com>
2021-04-09 11:13:07 +0200

    Small fix in matrix unit test

--- Tests/support/matrix_support.cpp
+++ Tests/support/matrix_support.cpp
@@ -34,15 +34,13 @@ void compare_arrays(Complex::Array2D &a, Complex::Array2D &b, float precision) {
 
   for (int r = 0; r < a.rows(); ++r) {
     for (int c = 0; c < a.columns(); ++c) {
-//      if (c == 0) {
-        INFO("(r, c): ( " << r << ", " << c << ")");
-        INFO(a[r][c].dump() << " == " << b[r][c].dump());
+      INFO("(r, c): ( " << r << ", " << c << ")");
+      INFO(a[r][c].dump() << " == " << b[r][c].dump());
 
-        // <= for dealing with precision 0.0f
+      // <= for dealing with precision 0.0f
 //        REQUIRE(abs(a[r][c].magnitude() - b[r][c].magnitude()) <= precision);
-        REQUIRE(abs(a[r][c].re() - b[r][c].re()) <= precision);
-        REQUIRE(abs(a[r][c].im() - b[r][c].im()) <= precision);
-//      }
+      REQUIRE(abs(a[r][c].re() - b[r][c].re()) <= precision);
+      REQUIRE(abs(a[r][c].im() - b[r][c].im()) <= precision);
     }
   }
 }
