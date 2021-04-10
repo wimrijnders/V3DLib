4e110dbf5dafbc08e7df870baed8d3c2312c5088
Wim Rijnders <wrijnders@gmail.com>
2021-04-10 06:27:25 +0200

    Added high precision sin/cos as Lib setting

--- Lib/LibSettings.cpp
+++ Lib/LibSettings.cpp
@@ -7,9 +7,10 @@ int const DEFAULT_HEAP_SIZE = 8*1024*1024;
 int const QPU_TIMEOUT = 10;
 
 struct SettingsInternal {
-  int  heap_size   = -1;        // bytes, size of shared (CPU-GPU) memory
-  int  qpu_timeout = -1;        // seconds, time to wait for response from QPU
-  bool use_tmu_for_load = true; // vc4 only, ignored for v3d. If false, use DMA
+  int  heap_size   = -1;                  // bytes, size of shared (CPU-GPU) memory
+  int  qpu_timeout = -1;                  // seconds, time to wait for response from QPU
+  bool use_tmu_for_load = true;           // vc4 only, ignored for v3d. If false, use DMA
+  bool use_high_precision_sincos = false; // If true, add extra precision to sin/cos calculation for function version
 } settings;
 
 }  // anon namespace
@@ -50,17 +51,11 @@ void LibSettings::heap_size(int val) {
 }
 
 
-bool LibSettings::use_tmu_for_load() {
-  return settings.use_tmu_for_load;
-}
+bool LibSettings::use_tmu_for_load()         { return settings.use_tmu_for_load; }
+void LibSettings::use_tmu_for_load(bool val) { settings.use_tmu_for_load = val; }
 
 
-void LibSettings::use_tmu_for_load(bool val) {
-  //std::string cmt;
-  //cmt << "LibSettings::use_tmu_for_load(" << val << ")";
-  //warning(cmt);
-  
-  settings.use_tmu_for_load = val;
-}
+bool LibSettings::use_high_precision_sincos()         { return settings.use_high_precision_sincos; }
+void LibSettings::use_high_precision_sincos(bool val) { settings.use_high_precision_sincos = val; }
 
 }  // namespace V3DLib
--- Lib/LibSettings.h
+++ Lib/LibSettings.h
@@ -10,10 +10,15 @@ class LibSettings {
 public:
   static int  qpu_timeout();
   static void qpu_timeout(int val);
+
   static int  heap_size();
   static void heap_size(int val);
+
   static bool use_tmu_for_load();
   static void use_tmu_for_load(bool val);
+
+  static bool use_high_precision_sincos();
+  static void use_high_precision_sincos(bool val);
 };
 
 }  // namespace V3DLib
