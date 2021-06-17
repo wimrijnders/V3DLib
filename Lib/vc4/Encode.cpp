4df9743066fd4294162a1abd80d10f95f4d40626
wimrijnders <wrijnders@gmail.com>
2021-06-17 08:55:37 +0200

    Started refactoring vc4 encode
    For better understanding.
    Interim save before vc4 hardware test

--- Lib/vc4/Encode.cpp
+++ Lib/vc4/Encode.cpp
@@ -9,6 +9,133 @@ namespace V3DLib {
 namespace vc4 {
 namespace {
 
+class vc4_Instr {
+public:
+  enum Tag {
+    NOP,
+    ROT,
+    ALU,
+    LI,
+    BR,
+    END,
+    SINC,
+    SDEC,
+
+    TMU0_TO_ACC4  // TODO RIP
+  };
+
+  uint32_t cond_add = 0;    // reused for LI, BR
+  uint32_t cond_mul = 0;
+  uint32_t waddr_add = 39;  // reused for LI
+  uint32_t waddr_mul = 39;
+
+  uint32_t addOp  = 0;
+  uint32_t mulOp  = 0;
+  uint32_t muxa   = 0;
+  uint32_t muxb   = 0;
+  uint32_t raddra = 39;
+  uint32_t raddrb = 0;
+
+  uint32_t li_imm = 0;  // Also used as BR target
+  uint32_t sema_id = 0;
+
+  void tag(Tag in_tag, bool imm = false) {
+    m_tag = in_tag;
+
+    switch(m_tag) {
+      case NOP:  // default
+      case LI:
+        break;  // as is
+
+      case ROT:
+        m_sig = 13;
+        break;
+
+      case ALU:
+        m_sig = imm?13:1;
+        break;
+
+      case BR:
+        m_sig = 15;
+        break;
+
+      case END:
+        m_sig = 3;
+        raddrb = 39;
+        break;
+
+      case TMU0_TO_ACC4:
+        m_sig = 10;
+        raddrb = 39;
+        break;
+
+      case SINC:
+      case SDEC:
+        m_sig = 0xe8;  // NOTE: not a single nibble like the rest
+        break;
+    };
+  }
+
+
+  void sf(bool val) { assert(m_tag != BR); m_sf = val; }
+  void ws(bool val) { assert(m_tag != BR); m_ws = val; }
+  void rel(bool val) { assert(m_tag == BR); m_rel = val; }
+
+
+  uint32_t high() const {
+    uint32_t ret = (m_sig << 28) | (waddr_add << 6) | waddr_mul;
+
+    if (m_tag == BR) {
+      ret |= (cond_add << 20)
+          |  (m_rel?(1 << 19):0);
+    } else if (m_tag == SINC || m_tag == SDEC) {
+      // NOTE sig shift different from rest, because sig not a nibble
+      ret = (m_sig << 24) | (waddr_add << 6) | waddr_mul;
+    } else if (m_tag != END && m_tag != TMU0_TO_ACC4) {
+      ret |= (cond_add << 17) | (cond_mul << 14) 
+          |  (m_sf?(1 << 13):0)
+          | (m_ws?(1 << 12):0);
+    }
+
+    return ret;
+  }
+
+
+  uint32_t low() const {
+    switch (m_tag) {
+      case NOP:
+        return  0;
+      case ROT:
+        return (mulOp << 29) | (raddra << 18) | (raddrb << 12);
+      case ALU:
+        return (mulOp << 29) | (addOp << 24) | (raddra << 18) | (raddrb << 12)
+                     | (muxa << 9) | (muxb << 6)
+                     | (muxa << 3) | muxb;
+      case END:
+      case TMU0_TO_ACC4:
+        return (raddra << 18) | (raddrb << 12);
+      case LI:
+      case BR:
+        return li_imm;
+      case SINC:
+        return sema_id;
+      case SDEC:
+        return (1 << 4) | sema_id;
+    };
+
+    assert(false);
+    return 0;
+  }
+
+private:
+  Tag m_tag = NOP;
+  uint32_t m_sig = 14;        // 0xe0000000
+
+  bool m_ws  = false;
+  bool m_sf  = false;
+  bool m_rel = false;
+};
+
 // ===============
 // Condition flags
 // ===============
@@ -275,13 +402,25 @@ void encodeInstr(Instr instr, uint32_t* high, uint32_t* low) {
       uint32_t sf   = (li.m_setCond.flags_set()? 1 : 0) << 13;
       *high         = 0xe0000000 | cond | ws | sf | waddr_add | waddr_mul;
       *low          = li.imm.encode();
+
+      ///////////////////
+      vc4_Instr vc4_instr;
+      vc4_instr.tag(vc4_Instr::LI);
+      vc4_instr.cond_add = encodeAssignCond(li.cond);
+      vc4_instr.waddr_add = encodeDestReg(li.dest, &file);
+      vc4_instr.sf(li.m_setCond.flags_set());
+      vc4_instr.ws(file != REG_A);
+      vc4_instr.li_imm = li.imm.encode();
+      assertq(*high == vc4_instr.high(), "Difference high output, LI");
+      assertq(*low == vc4_instr.low(), "Difference low output, LI");
+      ///////////////////
       return;
     }
 
     // Branch
     case BR: {
       // Register offset not yet supported
-      assert(! instr.BR.target.useRegOffset);
+      assert(!instr.BR.target.useRegOffset);
 
       uint32_t waddr_add = 39 << 6;
       uint32_t waddr_mul = 39;
@@ -289,6 +428,16 @@ void encodeInstr(Instr instr, uint32_t* high, uint32_t* low) {
       uint32_t rel  = (instr.BR.target.relative ? 1 : 0) << 19;
       *high = 0xf0000000 | cond | rel | waddr_add | waddr_mul;
       *low  = (uint32_t) 8*instr.BR.target.immOffset;
+
+      ///////////////////
+      vc4_Instr vc4_instr;
+      vc4_instr.tag(vc4_Instr::BR);
+      vc4_instr.cond_add = encodeBranchCond(instr.BR.cond);
+      vc4_instr.rel(instr.BR.target.relative);
+      vc4_instr.li_imm = 8*instr.BR.target.immOffset;
+      assertq(*high == vc4_instr.high(), "Difference high output, BR");
+      assertq(*low == vc4_instr.low(), "Difference low output, BR");
+      ///////////////////
       return;
     }
 
@@ -315,6 +464,23 @@ void encodeInstr(Instr instr, uint32_t* high, uint32_t* low) {
       uint32_t sf    = (alu.m_setCond.flags_set()? 1 : 0) << 13;
       *high          = sig | cond | ws | sf | waddr_add | waddr_mul;
 
+      ///////////////////
+      vc4_Instr vc4_instr;
+
+      if (alu.op.isMul()) {
+        vc4_instr.cond_mul  = encodeAssignCond(alu.cond);
+        vc4_instr.waddr_mul = dest;
+        vc4_instr.ws(file != REG_B);
+      } else {
+        vc4_instr.cond_add  = encodeAssignCond(alu.cond);
+        vc4_instr.waddr_add = dest;
+        vc4_instr.ws(file != REG_A);
+      }
+
+      vc4_instr.sf(alu.m_setCond.flags_set());
+      ///////////////////
+
+
       if (alu.op.isRot()) {
         assert(alu.srcA.is_reg() && alu.srcA.reg().tag == ACC && alu.srcA.reg().regId == 0);
         assert(alu.srcB.is_reg()? alu.srcB.reg().tag == ACC && alu.srcB.reg().regId == 5 : true);
@@ -329,8 +495,17 @@ void encodeInstr(Instr instr, uint32_t* high, uint32_t* low) {
           raddrb = 48 + n;
         }
 
+
         uint32_t raddra = 39;
         *low = mulOp | (raddrb << 12) | (raddra << 18);
+
+        ///////////////////
+        vc4_instr.tag(vc4_Instr::ROT);
+        vc4_instr.mulOp  = ALUOp(ALUOp::M_V8MIN).vc4_encodeMulOp();
+        vc4_instr.raddrb = raddrb;
+        assertq(*high == vc4_instr.high(), "Difference high output, ROT");
+        assertq(*low == vc4_instr.low(), "Difference low output, ROT");
+        ///////////////////
         return;
       } else {
         uint32_t mulOp = (alu.op.isMul() ? alu.op.vc4_encodeMulOp() : 0) << 29;
@@ -398,6 +573,18 @@ void encodeInstr(Instr instr, uint32_t* high, uint32_t* low) {
         *low = mulOp | addOp | (raddra << 18) | (raddrb << 12)
                      | (muxa << 9) | (muxb << 6)
                      | (muxa << 3) | muxb;
+      
+        ///////////////////
+        vc4_instr.tag(vc4_Instr::ALU, instr.hasImm());
+        vc4_instr.mulOp = (alu.op.isMul() ? alu.op.vc4_encodeMulOp() : 0);
+        vc4_instr.addOp = (alu.op.isMul() ? 0 : alu.op.vc4_encodeAddOp());
+        vc4_instr.raddra  = raddra;
+        vc4_instr.raddrb  = raddrb;
+        vc4_instr.muxa  = muxa;
+        vc4_instr.muxb  = muxb;
+        assertq(*high == vc4_instr.high(), "Difference high output, ALU");
+        assertq(*low == vc4_instr.low(), "Difference low output, ALU");
+        ///////////////////
         return;
       }
     }
@@ -412,6 +599,13 @@ void encodeInstr(Instr instr, uint32_t* high, uint32_t* low) {
       uint32_t sig = instr.tag == END ? 0x30000000 : 0xa0000000;
       *high  = sig | waddr_add | waddr_mul;
       *low   = raddra | raddrb;
+
+        ///////////////////
+        vc4_Instr vc4_instr;
+        vc4_instr.tag((instr.tag == END)?vc4_Instr::END:vc4_Instr::TMU0_TO_ACC4);
+        assertq(*high == vc4_instr.high(), "Difference high output, ALU");
+        assertq(*low == vc4_instr.low(), "Difference low output, ALU");
+        ///////////////////
       return;
     }
 
@@ -424,6 +618,14 @@ void encodeInstr(Instr instr, uint32_t* high, uint32_t* low) {
       uint32_t incOrDec = (instr.tag == SINC ? 0 : 1) << 4;
       *high = sig | waddr_add | waddr_mul;
       *low = incOrDec | instr.semaId;
+
+        ///////////////////
+        vc4_Instr vc4_instr;
+        vc4_instr.tag((instr.tag == SINC)?vc4_Instr::SINC:vc4_Instr::SDEC);
+        vc4_instr.sema_id = instr.semaId;
+        assertq(*high == vc4_instr.high(), "Difference high output, SINC SDEC", true);
+        assertq(*low == vc4_instr.low(), "Difference low output, SINC SDEC", true);
+        ///////////////////
       return;
     }
 
@@ -435,6 +637,11 @@ void encodeInstr(Instr instr, uint32_t* high, uint32_t* low) {
       uint32_t waddr_mul = 39;
       *high  = 0xe0000000 | waddr_add | waddr_mul;
       *low   = 0;
+      ///////////////////
+      vc4_Instr vc4_instr;
+      assertq(*high == vc4_instr.high(), "Difference high output, NO_OP");
+      assertq(*low == vc4_instr.low(), "Difference low output, NO_OP");
+      ///////////////////
       return;
     }
 
